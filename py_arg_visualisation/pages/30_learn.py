ce40cfbc5ae8f5e49747720ac2f42916bf680846
Daphne Odekerken <daphne.odekerken@uu.nl>
2023-05-24 14:15:05 +0200

    Improved layout learning environment.

--- py_arg_visualisation/pages/30_learn.py
+++ py_arg_visualisation/pages/30_learn.py
@@ -19,18 +19,20 @@ layout = html.Div(
                        value=list(exercise_dict.keys())[0],
                        id='exercise-choice-dropdown'),
             html.Br(),
-            dbc.Card([], id='explanation-html'),
+            dbc.Card([], id='explanation-html', className='border-0 bg-transparent'),
             html.Br(),
             dbc.Button('Generate exercise', id='practice-button', n_clicks=0),
-            html.Br(),
-            html.Br(),
-            dbc.Card([], id='exercise-text'),
-            dbc.Textarea(value='', id='answer-input-text-field', style={'width': '100%'}),
-            html.Br(),
-            dbc.Button('Check', id='check-button', n_clicks=0),
-            html.Br(),
-            html.Br(),
-            html.Div([], id='feedback-field'),
+            html.Div([
+                html.Br(),
+                html.Br(),
+                dbc.Card([], id='exercise-text', className='border-0 bg-transparent'),
+                dbc.Textarea(value='', id='answer-input-text-field', style={'width': '100%'}),
+                html.Br(),
+                dbc.Button('Check', id='check-button', n_clicks=0),
+                html.Br(),
+                html.Br(),
+                html.Div([], id='feedback-field'),
+            ], style={'display': 'none'}, id='30-exercise-div'),
             dcc.Store(id='solution-store', data=''),
         ])
     ]
@@ -48,23 +50,25 @@ def get_explanation_html(exercise_choice_value: str):
           Output('solution-store', 'data'),
           Output('answer-input-text-field', 'value'),
           Output('feedback-field', 'children'),
+          Output('30-exercise-div', 'style'),
           Input('practice-button', 'n_clicks'),
           Input('check-button', 'n_clicks'),
           State('exercise-choice-dropdown', 'value'),
           State('exercise-text', 'children'),
           State('answer-input-text-field', 'value'),
-          State('solution-store', 'data'))
+          State('solution-store', 'data'),
+          State('30-exercise-div', 'style'))
 def handle_button_click(_generate_button_clicks: int, _check_button_clicks: int,
                         exercise_choice_value: str, old_exercise_text: str,
-                        user_solution: str, pre_generated_solutions):
+                        user_solution: str, pre_generated_solutions, old_style: dict):
     button_clicked = dash.ctx.triggered_id
     if button_clicked == 'practice-button':
         exercise_set = exercise_dict[exercise_choice_value]
         exercise, solutions = exercise_set.generate_exercise_and_solutions()
         rendered_exercise = [html.B('Exercise'), exercise_set.render_exercise_instance(exercise)]
-        return rendered_exercise, solutions, '', ''
+        return rendered_exercise, solutions, '', '', {'display': 'block'}
     elif button_clicked == 'check-button':
         exercise_set = exercise_dict[exercise_choice_value]
         feedback_text = exercise_set.get_feedback(user_solution, pre_generated_solutions)
-        return old_exercise_text, pre_generated_solutions, user_solution, feedback_text
-    return '', [], '', ''
+        return old_exercise_text, pre_generated_solutions, user_solution, feedback_text, {'display': 'block'}
+    return '', [], '', '', old_style
