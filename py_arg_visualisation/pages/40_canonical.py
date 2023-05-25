77a2c7eb4e8de16143deb9edb16bb46c732e1c66
Daphne Odekerken <daphne.odekerken@uu.nl>
2023-05-25 16:29:45 +0200

    Added graphs to canonical output.

--- py_arg_visualisation/pages/40_canonical.py
+++ py_arg_visualisation/pages/40_canonical.py
@@ -1,29 +1,35 @@
 import dash
+import visdcc
 from dash import html, callback, Input, Output
 import dash_bootstrap_components as dbc
 
 from py_arg.abstract_argumentation_classes.argument import Argument
 from py_arg.algorithms.canonical_constructions import construct_af_adm, construct_af_grd, construct_af_naive, \
     construct_af_stage, construct_af_stb
+from py_arg_visualisation.functions.graph_data_functions.get_af_graph_data import get_argumentation_framework_graph_data
 
 dash.register_page(__name__, name='Canonical', title='Canonical')
 
 layout = html.Div(
     children=[
         html.H1('Create canonical AFs based on sets of extensions'),
-        html.B('Enter here your set of extensions'),
-        dbc.Textarea(id='40-extension-sets-textarea',
-                     placeholder='Put each set on a new line and represent sets as {X, Y, Z}.',
-                     style={'height': '200px'}),
-        html.B('Semantics'),
-        dbc.Select(options=[
-            {'label': 'Admissible', 'value': 'Admissible'},
-            {'label': 'Grounded', 'value': 'Grounded'},
-            {'label': 'Naive', 'value': 'Naive'},
-            {'label': 'Stage', 'value': 'Stage'},
-            {'label': 'Stable', 'value': 'Stable'},
-        ], value='Admissible', id='40-canonical-semantics'),
-        html.Div(id='40-canonical-output-div')
+        dbc.Row([
+            dbc.Col([
+                html.B('Enter here your set of extensions'),
+                dbc.Textarea(id='40-extension-sets-textarea',
+                             placeholder='Put each set on a new line and represent sets as {X, Y, Z}.',
+                             style={'height': '200px'}),
+                html.B('Semantics'),
+                dbc.Select(options=[
+                    {'label': 'Admissible', 'value': 'Admissible'},
+                    {'label': 'Grounded', 'value': 'Grounded'},
+                    {'label': 'Naive', 'value': 'Naive'},
+                    {'label': 'Stage', 'value': 'Stage'},
+                    {'label': 'Stable', 'value': 'Stable'},
+                ], value='Admissible', id='40-canonical-semantics'),
+            ]),
+            dbc.Col(html.Div(id='40-canonical-output-div'))
+        ])
     ]
 )
 
@@ -59,12 +65,13 @@ def get_canonical_argumentation_framework(extension_sets_str: str, semantics: st
         # TODO cf?
         raise NotImplementedError
 
+    graph_data = get_argumentation_framework_graph_data(af, None)
+
     return [
         html.B('Canonical argumentation framework'),
-        html.Br(),
-        html.P('AF = (A, D) where A = {{{a}}} and D = {{{d}}}.'.format(
+        dbc.Col(html.P('AF = (A, D) where A = {{{a}}} and D = {{{d}}}.'.format(
             a=', '.join(str(arg) for arg in af.arguments),
             d='; '.join('(' + str(defeat.from_argument) + ', ' + str(defeat.to_argument) + ')'
-                        for defeat in af.defeats)
-        ))
+                        for defeat in af.defeats)))),
+        dbc.Col(visdcc.Network(data=graph_data, options={'height': '500px'}, id='canonical-graph')),
     ]
