7d7300d19e5a499df31d791364682ab8bd8604d9
mattiberthold <mattiberthold@users.noreply.github.com>
2023-05-25 13:46:56 +0200

    canonical AF init
    tests don't assert yet, but run without error

---
+++ py_arg/algorithms/canonical_constructions/construct_af_cf.py
@@ -0,0 +1,14 @@
+
+from typing import Set
+
+import py_arg.algorithms.canonical_constructions.canonical_cf as canonical_cf
+from py_arg.abstract_argumentation_classes.abstract_argumentation_framework import AbstractArgumentationFramework
+import py_arg.algorithms.canonical_constructions.check_tight as check_tight
+import py_arg.algorithms.canonical_constructions.check_downward_closed as check_downward_closed
+
+
+@staticmethod
+def apply(extension_set: Set) -> AbstractArgumentationFramework:
+    if check_downward_closed.apply(extension_set) and check_tight.apply(extension_set):
+        return canonical_cf.apply(extension_set)
+    return AbstractArgumentationFramework('', arguments=[], defeats=[])
