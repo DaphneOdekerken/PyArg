809c23565b1b64992b0472f1f80298b901b30d0b
Daphne Odekerken <daphne.odekerken@uu.nl>
2023-06-01 08:00:57 +0200

    Table for canonical representations.

--- py_arg_visualisation/pages/40_canonical.py
+++ py_arg_visualisation/pages/40_canonical.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import dash
 import visdcc
 from dash import html, callback, Input, Output, State
@@ -5,10 +7,65 @@ import dash_bootstrap_components as dbc
 
 from py_arg.abstract_argumentation_classes.argument import Argument
 from py_arg.algorithms.canonical_constructions import construct_af_adm, construct_af_grd, construct_af_naive, \
-    construct_af_stage, construct_af_stb, construct_af_cf
+    construct_af_stage, construct_af_stb, construct_af_cf, check_tight
 from py_arg_visualisation.functions.graph_data_functions.get_af_graph_data import get_argumentation_framework_graph_data
 
 dash.register_page(__name__, name='Canonical', title='Canonical')
+
+properties_table = html.Div([html.Table([
+    html.Colgroup([
+        html.Col(style={}),
+        html.Col(id='40-downward-closed-column'),
+        html.Col(id='40-tight-column'),
+        html.Col(id='40-incomparable-column'),
+        html.Col(id='40-dcl-tight-column'),
+        html.Col(id='40-conflict-sensitive-column'),
+        html.Col(id='40-contains-empty-set-column'),
+        html.Col(id='40-one-extension-column'),
+        html.Col(style={}),
+    ]),
+    html.Tr([
+        html.Th(),
+        html.Th(html.Div(html.Span('Downward-closed')), className='rotate'),
+        html.Th(html.Div(html.Span('Tight')), className='rotate'),
+        html.Th(html.Div(html.Span('Incomparable')), className='rotate'),
+        html.Th(html.Div(html.Span('DCL-tight')), className='rotate'),
+        html.Th(html.Div(html.Span('Conflict-sensitive')), className='rotate'),
+        html.Th(html.Div(html.Span('Contains empty set')), className='rotate'),
+        html.Th(html.Div(html.Span('One extension')), className='rotate'),
+        html.Th(),
+    ]),
+    html.Tr([
+        html.Td('Conflict Free'), html.Td('✅'), html.Td('✅'), html.Td('❌'),
+        html.Td('❌'), html.Td('❌'), html.Td('❌'),
+        html.Td('❌'), html.Td(dbc.Button('Generate', id='40-generate-conflict-free-button'))
+    ]),
+    html.Tr([
+        html.Td('Admissible'), html.Td('✅'), html.Td('✅'), html.Td('❌'),
+        html.Td('❌'), html.Td('❌'), html.Td('❌'),
+        html.Td('❌'), html.Td(dbc.Button('Generate', id='40-generate-admissible-button'))
+    ]),
+    html.Tr([
+        html.Td('Grounded'), html.Td('✅'), html.Td('✅'), html.Td('❌'),
+        html.Td('❌'), html.Td('❌'), html.Td('❌'),
+        html.Td('❌'), html.Td(dbc.Button('Generate', id='40-generate-grounded-button'))
+    ]),
+    html.Tr([
+        html.Td('Stable'), html.Td('✅'), html.Td('✅'), html.Td('❌'),
+        html.Td('❌'), html.Td('❌'), html.Td('❌'),
+        html.Td('❌'), html.Td(dbc.Button('Generate', id='40-generate-stable-button'))
+    ]),
+    html.Tr([
+        html.Td('Naive'), html.Td('✅'), html.Td('✅'), html.Td('❌'),
+        html.Td('❌'), html.Td('❌'), html.Td('❌'),
+        html.Td('❌'), html.Td(dbc.Button('Generate', id='40-generate-naive-button'))
+    ]),
+    html.Tr([
+        html.Td('Stage'), html.Td('✅'), html.Td('✅'), html.Td('❌'),
+        html.Td('❌'), html.Td('❌'), html.Td('❌'),
+        html.Td('❌'), html.Td(dbc.Button('Generate', id='40-generate-stage-button'))
+    ]),
+])])
 
 layout = html.Div(
     children=[
@@ -19,15 +76,8 @@ layout = html.Div(
                 dbc.Textarea(id='40-extension-sets-textarea',
                              placeholder='Put each set on a new line and represent sets as {X, Y, Z}.',
                              style={'height': '200px'}),
-                html.B('Semantics'),
-                dbc.Select(options=[
-                    {'label': 'Conflict Free', 'value': 'Conflict_Free'},
-                    {'label': 'Admissible', 'value': 'Admissible'},
-                    {'label': 'Grounded', 'value': 'Grounded'},
-                    {'label': 'Stable', 'value': 'Stable'},
-                    {'label': 'Naive', 'value': 'Naive'},
-                    {'label': 'Stage', 'value': 'Stage'},
-                ], value='Admissible', id='40-canonical-semantics'),
+                html.B('Properties and semantics'),
+                properties_table,
             ]),
             dbc.Col(html.Div(id='40-canonical-output-div'))
         ])
@@ -35,15 +85,9 @@ layout = html.Div(
 )
 
 
-@callback(
-    Output('40-canonical-output-div', 'children'),
-    Input('40-extension-sets-textarea', 'value'),
-    Input('40-canonical-semantics', 'value'),
-    State('color-blind-mode', 'on'),
-    prevent_initial_call=True
-)
-def get_canonical_argumentation_framework(extension_sets_str: str, semantics: str, color_blind_mode: bool):
-
+def read_extension_sets_from_str(extension_sets_str: str):
+    if not extension_sets_str:
+        return set()
     extensions_set = extension_sets_str.split('\n')
     input_extension_set = set()
     for extension in extensions_set:
@@ -52,21 +96,79 @@ def get_canonical_argumentation_framework(extension_sets_str: str, semantics: st
         argument_set = frozenset(Argument(argument_str) for argument_str in argument_strs
                                  if argument_str)
         input_extension_set.add(argument_set)
+    return input_extension_set
 
-    if semantics == 'Admissible':
+
+@callback(
+    Output('40-downward-closed-column', 'style'),
+    Output('40-tight-column', 'style'),
+    Output('40-incomparable-column', 'style'),
+    Output('40-dcl-tight-column', 'style'),
+    Output('40-conflict-sensitive-column', 'style'),
+    Output('40-contains-empty-set-column', 'style'),
+    Output('40-one-extension-column', 'style'),
+    Output('40-generate-conflict-free-button', 'disabled'),
+    Output('40-generate-admissible-button', 'disabled'),
+    Output('40-generate-grounded-button', 'disabled'),
+    Output('40-generate-stable-button', 'disabled'),
+    Output('40-generate-naive-button', 'disabled'),
+    Output('40-generate-stage-button', 'disabled'),
+    Input('40-extension-sets-textarea', 'value')
+)
+def fill_properties_table(extension_sets_str: str):
+    input_extension_set = read_extension_sets_from_str(extension_sets_str)
+
+    # Test which properties hold
+    tight = check_tight.apply(input_extension_set)
+    if tight:
+        tight_style = {'background-color': '#AAAAAA'}
+    else:
+        tight_style = {}
+    # TODO also for other properties
+
+    # TODO: test which semantics can be applied
+
+    return {}, tight_style, {}, {}, {}, {}, {}, \
+        False, True, True, True, False, True
+
+
+@callback(
+    Output('40-canonical-output-div', 'children'),
+    State('40-extension-sets-textarea', 'value'),
+    Input('40-generate-conflict-free-button', 'n_clicks'),
+    Input('40-generate-admissible-button', 'n_clicks'),
+    Input('40-generate-grounded-button', 'n_clicks'),
+    Input('40-generate-stable-button', 'n_clicks'),
+    Input('40-generate-naive-button', 'n_clicks'),
+    Input('40-generate-stage-button', 'n_clicks'),
+    State('color-blind-mode', 'on'),
+    prevent_initial_call=True
+)
+def get_canonical_argumentation_framework(extension_sets_str: str,
+                                          _conflict_free_clicks: int,
+                                          _admissible_clicks: int,
+                                          _grounded_clicks: int,
+                                          _stable_clicks: int,
+                                          _naive_clicks: int,
+                                          _stage_clicks: int,
+                                          color_blind_mode: bool):
+    input_extension_set = read_extension_sets_from_str(extension_sets_str)
+
+    triggered_id = dash.ctx.triggered_id
+
+    if triggered_id == '40-generate-admissible-button':
         af = construct_af_adm.apply(input_extension_set)
-    elif semantics == 'Conflict_Free':
+    elif triggered_id == '40-generate-conflict-free-button':
         af = construct_af_cf.apply(input_extension_set)
-    elif semantics == 'Grounded':
+    elif triggered_id == '40-generate-grounded-button':
         af = construct_af_grd.apply(input_extension_set)
-    elif semantics == 'Naive':
+    elif triggered_id == '40-generate-naive-button':
         af = construct_af_naive.apply(input_extension_set)
-    elif semantics == 'Stage':
+    elif triggered_id == '40-generate-stage-button':
         af = construct_af_stage.apply(input_extension_set)
-    elif semantics == 'Stable':
+    elif triggered_id == '40-generate-stable-button':
         af = construct_af_stb.apply(input_extension_set)
     else:
-        # TODO cf?
         raise NotImplementedError
 
     graph_data = get_argumentation_framework_graph_data(af, None, color_blind_mode)
