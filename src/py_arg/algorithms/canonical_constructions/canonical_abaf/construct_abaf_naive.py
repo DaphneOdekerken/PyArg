29a639262a19df386afe564dc03bd4ed8b58c700
mattiberthold <mattiberthold@users.noreply.github.com>
2023-08-02 17:39:37 +0200

    updated canonical naive abaf
    apparently canonical naive abaf are constructed via the st-construction, not the cf-construction

--- src/py_arg/algorithms/canonical_constructions/canonical_abaf/construct_abaf_naive.py
+++ src/py_arg/algorithms/canonical_constructions/canonical_abaf/construct_abaf_naive.py
@@ -5,11 +5,11 @@ import py_arg.algorithms.canonical_constructions.check_incomparable as check_inc
 import py_arg.algorithms.canonical_constructions.aux_operators as aux
 import py_arg.algorithms.canonical_constructions.check_non_empty as check_non_empty
 from py_arg.aba_classes.aba_framework import ABAF
-from py_arg.algorithms.canonical_constructions.canonical_abaf import canonical_cf
+from py_arg.algorithms.canonical_constructions.canonical_abaf import canonical_st
 
 
 @staticmethod
 def apply(extension_set: Set) -> ABAF:
     if check_non_empty.apply(extension_set) and check_incomparable.apply(extension_set):
-        return canonical_cf.apply(aux.dcl(extension_set))
+        return canonical_st.apply(extension_set)
     return ABAF(set(), set(), set(), {})
