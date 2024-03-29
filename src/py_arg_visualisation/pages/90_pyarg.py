3e96e0708f2ef16bbf7bba0d75069b06999a9d83
Daphne Odekerken <daphne.odekerken@uu.nl>
2024-03-29 17:44:55 +0100

    Contributors update

--- src/py_arg_visualisation/pages/90_pyarg.py
+++ src/py_arg_visualisation/pages/90_pyarg.py
@@ -9,17 +9,31 @@ layout = html.Div(children=[
            'problems in computational argumentation.'),
     html.P(['If you have any questions, feedback or ambitions to contribute, '
             'feel free to contact ',
-            html.A('Daphne Odekerken',
+            html.A('Daphne Odekerken (D.Odekerken@UU.nl)',
                    href='mailto:d.odekerken@uu.nl?subject=PyArg'),
             '.']),
     html.P('Contributors:'),
     html.Ul([
-        html.Li(html.A('Daphne Odekerken',
-                       href='https://webspace.science.uu.nl/~3827887/')),
         html.Li(html.A(
             'Matti Berthold',
-            href='https://www.informatik.uni-leipzig.de/~berthold/')),
+            href='https://www.informatik.uni-leipzig.de/~berthold/',
+            target='_blank')),
         html.Li(html.A('AnneMarie Borg',
-                       href='https://annemarieborg.nl/')),
+                       href='https://annemarieborg.nl/',
+                       target='_blank')),
+        html.Li(html.A('Jonas Klein',
+                       href='https://www.fernuni-hagen.de/aig/en/team/jonas'
+                            '.klein.shtml',
+                       target='_blank')),
+        html.Li(html.A('Bertram Ludäscher',
+                       href='https://cs.illinois.edu/about/people/faculty/'
+                            'ludaesch',
+                       target='_blank')),
+        html.Li(html.A('Daphne Odekerken',
+                       href='https://webspace.science.uu.nl/~3827887/',
+                       target='_blank')),
+        html.Li(html.A('Yilin Xia',
+                       href='https://yilinxia.com/',
+                       target='_blank'))
     ])
 ])
